aed6e3b2ef118bbdcdaaabb29ce0d7e3ea3ceffe
fatahilah-mr <fatahilah.f10@gmail.com>
2026-07-22 23:26:10 +0700

    feat: integrate @mui/icons-material and replace text arrows with MUI icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://blog.fatahmr.my.id',
   integrations: [
     sitemap(),
+    react(),
   ],
   markdown: {
     shikiConfig: {
